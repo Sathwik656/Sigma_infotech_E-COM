c9592d67ce420ad1a16179d65480aebf048ca712
Sathwik <112880525+Sathwik656@users.noreply.github.com>
2026-07-12 23:02:55 +0530

    Deploy-1

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,3 @@
-'use strict';
-
-require('dotenv').config();
-const { validateEnv } = require('../src/config/env');
-
-// Validate environment variables before anything else starts
-validateEnv();
-
 const app = require('../src/app');
 
 module.exports = app;
